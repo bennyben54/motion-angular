24601eb78631246c94b4bcaf85133ed87b90c6bd
Benny Ben <bennyben54@gmail.com>
2019-08-15 15:41:59 +0200

    [CHORE] (server-address) change server address

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
   servers: {
-    oauth: 'http://192.168.1.76:8090/oauth',
-    userApi: 'http://192.168.1.76:8090/api/user',
-    camera1: 'http://192.168.1.76:8081',
-    camera2: 'http://192.168.1.76:8082'
+    oauth: 'http://192.168.0.76:8090/oauth',
+    userApi: 'http://192.168.0.76:8090/api/user',
+    camera1: 'http://192.168.0.76:8081',
+    camera2: 'http://192.168.0.76:8082'
   },
   oauth: {
     appName: 'beo-app-client',
